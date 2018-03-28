9f50b7d713c126412b084f7405fc5e9023abfacd
Xtremind <guillaume.dagraca@gmail.com>
2018-03-28 18:03:31 +0000

    fix given card

--- server/dto/gameDTO.js
+++ server/dto/gameDTO.js
@@ -1,17 +1,12 @@
 var GameDTO = function(scoringGame, playedCards, givenCards, action){
   this.scoringGame = scoringGame; //last computed score
   this.playedCards = [];
-  this.givenCards = [];
+  this.givenCards = givenCards;
   this.action = action;      //action for the player to do
 
   playedCards.forEach((element, key) => {
     this.playedCards.push({key: element}) //all played cards in play
   });
-  
-  currentTurn.forEach((element, key) => {
-    this.currentTurn.push({key: element}) //cards to display on board
-  });
-
 }
 
 GameDTO.prototype = {
